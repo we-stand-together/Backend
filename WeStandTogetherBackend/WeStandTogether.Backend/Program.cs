f7811873d1bd0eceb962f57899567ef4a6097f3b
stav10 <stavchek2@gmail.com>
2023-05-23 14:58:26 +0300

    Added package refrences

--- WeStandTogetherBackend/WeStandTogether.Backend/Program.cs
+++ WeStandTogetherBackend/WeStandTogether.Backend/Program.cs
@@ -1,10 +1,20 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
-
-
-
-
-
+var builder = WebApplication.CreateBuilder(args);
+ 
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration.GetValue<string>("JwtBearer")
+    }
+});
 
 var app = builder.Build();
 
