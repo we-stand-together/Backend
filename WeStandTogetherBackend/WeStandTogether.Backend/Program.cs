8e41f5e9780c3e76fac847ef59e08a77a5faf9fd
stav10 <stavchek2@gmail.com>
2023-05-23 14:58:54 +0300

    Remove irrelevant code

--- WeStandTogetherBackend/WeStandTogether.Backend/Program.cs
+++ WeStandTogetherBackend/WeStandTogether.Backend/Program.cs
@@ -3,18 +3,7 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
  
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidIssuer = builder.Configuration.GetValue<string>("JwtBearer")
-    }
-});
+
 
 var app = builder.Build();
 
