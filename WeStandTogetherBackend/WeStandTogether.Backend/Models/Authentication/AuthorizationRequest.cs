d839fcea7239fb4224bd324d075c148d56ac83a2
Ziv18 <134368909+Ziv18@users.noreply.github.com>
2023-05-23 20:25:54 +0300

    Revert "Fix/form "

--- WeStandTogetherBackend/WeStandTogether.Backend/Models/Authentication/AuthorizationRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WeStandTogether.Backend.Models.Authentication;
-
-public class AuthorizationRequest
-{
-    public string PhoneNumber { get; set; }
-    public string Password { get; set; }
-}
