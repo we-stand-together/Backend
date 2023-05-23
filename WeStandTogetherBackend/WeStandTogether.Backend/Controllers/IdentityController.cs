0266e51186933fe1dbb9563c730f1382de66e210
stav10 <stavchek2@gmail.com>
2023-05-23 17:26:51 +0300

    Added jwt token generation login controller

---
+++ WeStandTogetherBackend/WeStandTogether.Backend/Controllers/IdentityController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WeStandTogether.Backend.Models.Authentication;
+
+namespace WeStandTogether.Backend.Controllers
+{
+    [ApiController]
+    public class IdentityController: ControllerBase
+    {
+        private const string TokenSecret = "InRealScenariosThisShouldBeStoredSecurely!";
+        private static readonly TimeSpan TokenLifeTime = TimeSpan.FromHours(24);
+
+        [HttpPost("login")]
+        public IActionResult GenerateToken([FromBody] LoginRequest request)
+        {
+            Console.WriteLine("HI");
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(TokenSecret);
+
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new(JwtRegisteredClaimNames.Sub, request.PhoneNumber),
+                new("phone_number", request.PhoneNumber)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.Add(TokenLifeTime),
+                Issuer = "WeStandTogether.com",
+                Audience = "WeStandTogether.com",
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwt = tokenHandler.WriteToken(token);
+
+            return Ok(jwt);
+        }
+    }
+}
--- WeStandTogetherBackend/WeStandTogether.Backend/Program.cs
+++ WeStandTogetherBackend/WeStandTogether.Backend/Program.cs
@@ -1,13 +1,39 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
- 
+var builder = WebApplication.CreateBuilder();
 
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration.GetValue<string>("JwtSettings:Issuer"),
+        ValidAudience = builder.Configuration.GetValue<string>("JwtSettings:Audience"),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JwtSettings:Key"))),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/", () => "Hello world");
+app.MapControllers();
+
+app.MapGet("/hi", () => "helloworld");
 
 app.Run();
