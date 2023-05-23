27c2b5d1c71fedd7d34f15e6114c31a0b6432733
blitz <zivbli2003@gmail.com>
2023-05-23 15:42:02 +0300

    Add dapper context, Fix gitignore

---
+++ WeStandTogetherBackend/WeStandTogether.Dapper/DapperContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace WeStandTogether.Dapper
+{
+    public class DapperContext
+    {
+        private readonly string _connectionString;
+
+        public DapperContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
